f2eca299b6df13ab68f31833d3703915ae99bc52
jriddell <jriddell@033e4343-f3fb-0310-8ec1-c1a524954513>
2005-09-20 17:05:34 +0000

    Add missing moc includes

--- trunk/katapult/common/katapultdisplay.cpp
+++ trunk/katapult/common/katapultdisplay.cpp
@@ -18,6 +18,8 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include "katapultdisplay.moc"
+
 #include "katapultdisplay.h"
 
 KatapultDisplay::KatapultDisplay(const char *name, WFlags f)
